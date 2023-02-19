7b129300fe88200601f157cfcda9b02178131cb0
khersameesh24 <khersameesh24@gmail.com>
2023-02-19 12:22:37 +0000

    exit if file format not supported

--- FastqStats/src/fastq_reader.py
+++ FastqStats/src/fastq_reader.py
@@ -13,24 +13,27 @@ from typing import Iterator
 
 
 # @track
-def read_fastq(file_path) -> Iterator[str]:
+def read_fastq(file_path: str) -> Iterator[str]:
     """
     Read input fastq file path and return a generator
     of FastqObj
     Args: filepath - path to the fastq file
     """
-
-    print("Input Fastq File :", os.path.basename(file_path))
     try:
         file_ext = os.path.splitext(file_path)[1]
 
         if file_ext == ".gz":
             filehandle = gzip.open(file_path, 'r')
+        
         elif file_ext == ".fastq" or file_ext == ".fq":
             filehandle = open(file_path, 'rt')
         else:
             print(
-                "Check if the correct file was passed. Supported formats `.fastq` & `.fastq.gz`")
+                "Check if the correct file was passed. Supported formats `.fastq`, `.fq` & `.fastq.gz`")
+            sys.exit(1)
+
+        print(f"Input Fastq File : {os.path.basename(file_path)}")
+        print(f"Fastq File Size  : {os.path.getsize(file_path)/(1024*1024*1024):.2f}G")
 
         for seq_iden, raw_seq, desc, qual_val in zip(*[filehandle] * 4):
 
